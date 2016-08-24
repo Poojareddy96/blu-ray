7a112dca3a3ce6c1dad4f6609f32421e2bd91aba
Pooja <poojaprabakaran96@gmail.com>
2016-08-24 18:16:23 +0530

    Add files via upload

---
+++ Reversedigits.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Reversedigits
+{
+public static void main(String args[])
+{
+long n;
+Scanner sc=new Scanner(System.in);
+n=sc.nextLong();
+Reversedigits r=new Reversedigits();
+System.out.print(r.fun(n));
+}
+public long fun(long n)
+{
+long rev=0;
+while(n!=0)
+{
+ long a=n%10;
+ rev=(rev*10)+a;
+ n=n/10;
+}
+return(rev);
+}
+}
