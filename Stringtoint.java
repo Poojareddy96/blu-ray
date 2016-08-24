522adb0320b9cbb47e23b20d1ffd7e5931762121
Pooja <poojaprabakaran96@gmail.com>
2016-08-24 18:45:28 +0530

    Add files via upload

---
+++ Stringtoint.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Stringtoint
+{
+public static void main(String args[])
+{
+int i;
+Scanner sc=new Scanner(System.in);
+String s=sc.nextLine();
+i=Integer.parseInt(s);
+System.out.print(i);
+}
+}
