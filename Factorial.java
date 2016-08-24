6a6f39056053662b8202d991e943a130255aa194
Pooja <poojaprabakaran96@gmail.com>
2016-08-24 17:47:26 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Factorial
+{
+public static void main(String args[])
+{
+int i,n,fact=1;
+Scanner sc=new Scanner(System.in);
+n=sc.nextInt();
+for(i=1;i<n;i++)
+{
+ fact+=fact*i;
+}
+System.out.print(fact);
+}
+}
